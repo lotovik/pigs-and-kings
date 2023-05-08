6a51c348435b9a396405f241b47912d282150f60
lotovik <contact@victorleao.com>
2023-05-08 14:47:49 -0300

    Ex1: Done, Ex2: WIP

--- source/Interface/SaveMenu/SaveMenu.gd
+++ source/Interface/SaveMenu/SaveMenu.gd
@@ -8,15 +8,25 @@ func _input(event):
 		get_tree().paused = not get_tree().paused
 		$SaveButton.grab_focus()
 
-
 func _on_save_button_pressed():
-	pass
-
+	save_scene()
 
 func _on_main_menu_button_pressed():
+	save_scene()
 	get_tree().change_scene_to_file(main_menu_scene_path)
 
-
 func _on_quit_button_pressed():
-	# Save before quitting
+	save_scene()
 	get_tree().quit()
+
+func save_scene():
+	var directory_save_path : String = "user://"
+	var dir_access : DirAccess = DirAccess.open(directory_save_path)
+	dir_access.make_dir("user://saves")
+	
+	var save_file_path : String = "user://saves/save_01.sav"
+	
+	var file_access : FileAccess = FileAccess.open(save_file_path, FileAccess.WRITE)
+	var scene_path : String = owner.scene_file_path
+	file_access.store_string(scene_path)
+	file_access.close()
--- source/Screens/MainMenu.gd
+++ source/Screens/MainMenu.gd
@@ -1,19 +1,19 @@
 extends Control
 
 @export_file("*.tscn") var start_scene_path = "res://Levels/Level1.tscn"
-
+var save_file_path : String = "user://saves/save_01.sav"
 
 func _ready():
 	$StartButton.grab_focus()
-
+	
+	if FileAccess.file_exists(save_file_path):
+		$ContinueButton.disabled = false
 
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file(start_scene_path)
 
-
 func _on_continue_button_pressed():
-	pass
-
+	get_tree().change_scene_to_file(save_file_path)
 
 func _on_quit_button_pressed():
 	get_tree().quit()
